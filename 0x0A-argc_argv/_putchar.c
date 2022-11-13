a348916f8bde9ceb7f147b72a819752c4afa923d
alphy1997 <alphonceomondi64@gmail.com>
2022-11-13 13:30:42 -0800

    Write a program that prints its name, followed by a new line.
    If you rename the program, it will print the new name, without having to compile it again
    You should not remove the path before the name of the program

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - writes a chacter c to stdout
+ * @c: the character to print
+ * Return: on success 1, otherwise -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
