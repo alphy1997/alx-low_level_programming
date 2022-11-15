83cb93ff1219797242cd292c9489753e4392a02a
alphy1997 <alphonceomondi64@gmail.com>
2022-11-15 13:53:26 -0800

    Create a header file that defines a macro named SIZE as an abbreviation for the token 1024.

---
+++ 0x0D-preprocessor/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the charcter c to stdout
+ * @c: the character to print
+ * Return: on success 1 otherwise -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
